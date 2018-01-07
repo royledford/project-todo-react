ca69b112da55f0620ee6364eee3d67dbf6d929c5
roy ledford <bluesixty@gmail.com>
2018-01-07 12:48:57 -0500

    Remove router and fix footer bug - Removed react-router as don't need it now - Change footer to absolute position

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import AppContainer from './components/AppContainer'
 
 import './App.css'
 
 class App extends Component {
   render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={AppContainer} />
-        </Switch>
-      </Router>
-    )
+    return <AppContainer />
   }
 }
 
